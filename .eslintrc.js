ac2a687d2d2547df91e85ef4f2707f681484948e
ky0yk <40429527+ky0yk@users.noreply.github.com>
2023-08-12 07:09:32 +0900

    .eslintrc.jsを修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,16 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    extends: [
-      'eslint:recommended',
-      'plugin:@typescript-eslint/recommended',
-      'prettier',
-      'prettier/@typescript-eslint',
-    ],
-    plugins: ['@typescript-eslint', 'prettier'],
-    env: {
-      node: true,
-      es6: true,
-    },
-    rules: {
-      'prettier/prettier': 'error',
-    },
-  };
-  
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint', 'prettier'],
+  env: {
+    node: true,
+    es6: true,
+  },
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
