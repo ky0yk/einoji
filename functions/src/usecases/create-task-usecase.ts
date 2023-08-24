3516e943dbadde1c51ab23ed6a504bccb5b3568b
ky0yk <40429527+ky0yk@users.noreply.github.com>
2023-08-24 22:31:19 +0900

    lintに対応

--- functions/src/usecases/create-task-usecase.ts
+++ functions/src/usecases/create-task-usecase.ts
@@ -3,10 +3,7 @@ import {
   fetchTaskById,
   createTask as ddbCreateTask,
 } from '../infrastructure/ddb/tasks-table';
-import {
-  TaskNotFoundError,
-  TaskUnknownError,
-} from '../domain/errors/task-errors';
+import { TaskNotFoundError } from '../domain/errors/task-errors';
 import { useCaseFactory } from './factory/usecase-factory';
 import { CreateTaskRequest } from '../handlers/request_schemas/create-task-request';
 
