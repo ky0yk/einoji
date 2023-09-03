30647a226c9d1e846804b69291ba9153de59c287
ky0yk <40429527+ky0yk@users.noreply.github.com>
2023-09-03 10:58:19 +0900

    validatorを修正

--- functions/src/handlers/http/validators.ts
+++ functions/src/handlers/http/validators.ts
@@ -62,31 +62,36 @@ export const validateEventPathParameters = (
   return eventResult.data;
 };
 
-const RELEVANT_ISSUE_CODES = [
+const VALUE_RELATED_ISSUE_CODES = [
   ZodIssueCode.too_big,
   ZodIssueCode.too_small,
 ] as const;
 
-type RelevantIssueCodes = (typeof RELEVANT_ISSUE_CODES)[number];
+// NOTE: 定数から型を生成
+type ValueRelatedIssueCodes = (typeof VALUE_RELATED_ISSUE_CODES)[number];
 
-const isRelevantError = (code: any): code is RelevantIssueCodes => {
-  return RELEVANT_ISSUE_CODES.includes(code);
+const isValueRelatedError = (
+  code: ZodIssueCode,
+): code is ValueRelatedIssueCodes => {
+  return VALUE_RELATED_ISSUE_CODES.some(
+    (relevantCode) => relevantCode === code,
+  );
 };
 
 const createValidationError = (error: z.ZodError): AppError => {
   const errorDetails = error.errors.map((e) => e.message).join(', ');
 
-  const hasInvalidValue = error.errors.some((err) => isRelevantError(err.code));
-
-  if (hasInvalidValue) {
-    return new AppError(
-      ErrorCode.INVALID_PAYLOAD_VALUE,
-      `Invalid value in the request payload. Details: ${errorDetails}`,
-    );
-  } else {
-    return new AppError(
-      ErrorCode.INVALID_PAYLOAD_FORMAT,
-      `Payload format is incorrect. Details: ${errorDetails}`,
-    );
+  for (const err of error.errors) {
+    if (isValueRelatedError(err.code)) {
+      return new AppError(
+        ErrorCode.INVALID_PAYLOAD_VALUE,
+        `Invalid value in the request payload. Details: ${errorDetails}`,
+      );
+    }
   }
+
+  return new AppError(
+    ErrorCode.INVALID_PAYLOAD_FORMAT,
+    `Payload format is incorrect. Details: ${errorDetails}`,
+  );
 };
