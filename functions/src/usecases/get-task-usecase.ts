092791c044e6dceed76f98894c805cbeebf69b58
ky0yk <40429527+ky0yk@users.noreply.github.com>
2023-09-13 20:18:56 +0900

    ユースケースを修正

--- functions/src/usecases/get-task-usecase.ts
+++ functions/src/usecases/get-task-usecase.ts
@@ -1,15 +1,15 @@
-import { Task, toTask } from '../domain/task';
-import { getTaskItemById } from '../infrastructure/ddb/task-repository';
+import { Task } from '../domain/task';
 import { TaskNotFoundError } from '../domain/errors/task-errors';
 import { useCaseFactory } from './factory/usecase-factory';
+import { taskRepository } from '../infrastructure/ddb/task-repository';
 
 const getTask = async (taskId: string): Promise<Task> => {
-  const taskRecord = await getTaskItemById(taskId);
-  if (!taskRecord) {
+  const task = await taskRepository.getById(taskId);
+  if (!task) {
     throw new TaskNotFoundError(`The task not found. Task ID: ${taskId}`);
   }
 
-  return toTask(taskRecord);
+  return task;
 };
 
 export const getTaskUseCase = useCaseFactory('getTask', getTask);
