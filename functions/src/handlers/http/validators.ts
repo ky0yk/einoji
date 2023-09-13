9face281332ed7d53e713a3cb67f8880632a14c1
ky0yk <40429527+ky0yk@users.noreply.github.com>
2023-09-13 20:58:13 +0900

    validatorを修正

--- functions/src/handlers/http/validators.ts
+++ functions/src/handlers/http/validators.ts
@@ -1,65 +1,69 @@
-import { ZodIssueCode, ZodObject, ZodRawShape, z } from 'zod';
+import { ZodIssueCode, ZodSchema, ZodType, z } from 'zod';
 import { AppError } from '../../common/errors/app-errors';
 import { ErrorCode } from '../../common/errors/error-codes';
 import { APIGatewayEvent } from 'aws-lambda';
 
-const EventSchema = z.object({
-  body: z.string(),
-});
-
-const EventWithPathParamSchema = z.object({
-  pathParameters: z.object({ id: z.string().uuid() }),
-});
-
-type Event = z.infer<typeof EventSchema>;
-type EventWithPathParams = z.infer<typeof EventWithPathParamSchema>;
-
-export const validateEvent = (event: APIGatewayEvent): Event => {
-  const eventResult = EventSchema.safeParse(event);
-
-  if (!eventResult.success) {
-    throw createValidationError(eventResult.error);
-  }
-
-  return eventResult.data;
-};
-
-export const validateBody = <T extends ZodRawShape>(
-  schema: ZodObject<T>,
-  jsonBody: string,
-): z.infer<typeof schema> => {
-  let body;
+export const validateBody = <T>(
+  bodySchema: ZodSchema<T>,
+  event: APIGatewayEvent,
+): T => {
+  let parsedBody: T;
   try {
-    body = JSON.parse(jsonBody);
-  } catch (e: unknown) {
+    parsedBody = JSON.parse(event.body!);
+  } catch (error) {
     throw new AppError(
       ErrorCode.INVALID_PAYLOAD_FORMAT,
-      'Payload format is incorrect. ',
+      'Failed to parse the request body.',
     );
   }
 
-  const bodyResult = schema.safeParse(body);
-
-  if (!bodyResult.success) {
-    throw createValidationError(bodyResult.error);
+  const parsedResult = bodySchema.safeParse(parsedBody);
+  if (!parsedResult.success) {
+    throw createValidationError(parsedResult.error);
   }
 
-  return bodyResult.data;
+  return parsedResult.data;
 };
 
-export const validateEventPathParameters = (
+export const validatePathParams = <T>(
+  pathParamsSchema: ZodSchema<T>,
   event: APIGatewayEvent,
-): EventWithPathParams => {
-  const eventResult = EventWithPathParamSchema.safeParse(event);
-
-  if (!eventResult.success) {
+): T => {
+  if (!event.pathParameters) {
     throw new AppError(
       ErrorCode.INVALID_PATH_PARAMETER,
-      `Invalid or missing parameters. Details: ${eventResult.error.message}}`,
+      'Path parameters are missing.',
     );
   }
 
-  return eventResult.data;
+  const parsedResult = pathParamsSchema.safeParse(event.pathParameters);
+
+  if (!parsedResult.success) {
+    throw new AppError(
+      ErrorCode.INVALID_PATH_PARAMETER,
+      'Path parameters are invalid.',
+    );
+  }
+
+  return parsedResult.data;
+};
+
+type BodyAndPathParams<T, U> = {
+  body: T;
+  pathParameters: U;
+};
+export const validateBodyAndPathParams = <T, U>(
+  event: APIGatewayEvent,
+  bodySchema: ZodType<T>,
+  pathParamsSchema: ZodType<U>,
+): BodyAndPathParams<T, U> => {
+  const body = validateBody(bodySchema, event);
+  const pathParams = validatePathParams(pathParamsSchema, event);
+
+  return {
+    body: body,
+    pathParameters: pathParams,
+  };
 };
 
 const VALUE_RELATED_ISSUE_CODES = [
@@ -81,17 +85,16 @@ const isValueRelatedError = (
 const createValidationError = (error: z.ZodError): AppError => {
   const errorDetails = error.errors.map((e) => e.message).join(', ');
 
-  for (const err of error.errors) {
-    if (isValueRelatedError(err.code)) {
-      return new AppError(
-        ErrorCode.INVALID_PAYLOAD_VALUE,
-        `Invalid value in the request payload. Details: ${errorDetails}`,
-      );
-    }
-  }
-
-  return new AppError(
-    ErrorCode.INVALID_PAYLOAD_FORMAT,
-    `Payload format is incorrect. Details: ${errorDetails}`,
+  const isValueError = error.errors.some((err) =>
+    isValueRelatedError(err.code),
   );
+
+  const errorCode = isValueError
+    ? ErrorCode.INVALID_PAYLOAD_VALUE
+    : ErrorCode.INVALID_PAYLOAD_FORMAT;
+  const errorMessage = isValueError
+    ? `Invalid value in the request payload. Details: ${errorDetails}`
+    : `Payload format is incorrect. Details: ${errorDetails}`;
+
+  return new AppError(errorCode, errorMessage);
 };
