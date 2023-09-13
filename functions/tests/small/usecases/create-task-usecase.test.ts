2190ee8390af3adbf7cb230751caaeb66d284375
ky0yk <40429527+ky0yk@users.noreply.github.com>
2023-09-13 20:19:10 +0900

    テストを修正

--- functions/tests/small/usecases/create-task-usecase.test.ts
+++ functions/tests/small/usecases/create-task-usecase.test.ts
@@ -1,22 +1,16 @@
-import { Task, toTask } from '../../../src/domain/task';
-import { TaskItem } from '../../../src/infrastructure/ddb/schemas/task-item';
+import { Task } from '../../../src/domain/task';
 import { createTaskUseCase } from '../../../src/usecases/create-task-usecase';
-import { CreateTaskRequest } from '../../../src/handlers/http/requestSchemas/create-task-request';
 import { ErrorCode } from '../../../src/common/errors/error-codes';
-import {
-  createTaskItem,
-  getTaskItemById,
-} from '../../../src/infrastructure/ddb/task-repository';
+import { taskRepository } from '../../../src/infrastructure/ddb/task-repository';
 import { AppError } from '../../../src/common/errors/app-errors';
+import { CreateTaskRequest } from '../../../src/handlers/http/requestSchemas/create-task-request';
 
-jest.mock('../../../src/infrastructure/ddb/tasks-table');
-jest.mock('../../../src/domain/task');
+jest.mock('../../../src/infrastructure/ddb/task-repository');
 
 describe('createTaskUseCase', () => {
   beforeEach(() => {
-    (createTaskItem as jest.Mock).mockClear();
-    (getTaskItemById as jest.Mock).mockClear();
-    (toTask as jest.Mock).mockClear();
+    (taskRepository.create as jest.Mock).mockClear();
+    (taskRepository.getById as jest.Mock).mockClear();
   });
 
   const dummyCreateTaskRequest: CreateTaskRequest = {
@@ -46,35 +40,21 @@ describe('createTaskUseCase', () => {
 
   const validTaskId = 'valid-task-id';
 
-  const dummyTaskItem: TaskItem = {
-    userId: '1a7244c5-06d3-47e2-560e-f0b5534c8246',
-    taskId: 'f0f8f5a0-309d-11ec-8d3d-0242ac130003',
-    title: 'スーパーに買い物に行く',
-    completed: false,
-    description: '牛乳と卵を買う',
-    createdAt: '2021-06-22T14:24:02.071Z',
-    updatedAt: '2021-06-22T14:24:02.071Z',
-  };
-
   test.each`
     request                                     | task
     ${dummyCreateTaskRequest}                   | ${dummyTask}
     ${dummyCreateTaskRequestWithoutDescription} | ${dummyTaskWithoutDescription}
   `('should create a task successfully', async ({ request, task }) => {
-    (createTaskItem as jest.Mock).mockResolvedValue(validTaskId);
-    (getTaskItemById as jest.Mock).mockResolvedValue(dummyTaskItem);
-    (toTask as jest.Mock).mockReturnValue(task);
+    (taskRepository.create as jest.Mock).mockResolvedValue(validTaskId);
+    (taskRepository.getById as jest.Mock).mockResolvedValue(task);
 
     const result = await createTaskUseCase(request);
 
-    expect(createTaskItem).toHaveBeenCalledTimes(1);
-    expect(createTaskItem).toHaveBeenCalledWith(request);
+    expect(taskRepository.create).toHaveBeenCalledTimes(1);
+    expect(taskRepository.create).toHaveBeenCalledWith(request);
 
-    expect(getTaskItemById).toHaveBeenCalledTimes(1);
-    expect(getTaskItemById).toHaveBeenCalledWith(validTaskId);
-
-    expect(toTask).toHaveBeenCalledTimes(1);
-    expect(toTask).toHaveBeenCalledWith(dummyTaskItem);
+    expect(taskRepository.getById).toHaveBeenCalledTimes(1);
+    expect(taskRepository.getById).toHaveBeenCalledWith(validTaskId);
 
     expect(result).toEqual(task);
   });
@@ -82,17 +62,17 @@ describe('createTaskUseCase', () => {
   test('should throw AppError with TASK_NOT_FOUND if TaskItem is not found', async () => {
     const invalidTaskId = 'invalid-task-id';
 
-    (createTaskItem as jest.Mock).mockResolvedValue(invalidTaskId);
-    (getTaskItemById as jest.Mock).mockResolvedValue(null);
+    (taskRepository.create as jest.Mock).mockResolvedValue(invalidTaskId);
+    (taskRepository.getById as jest.Mock).mockResolvedValue(null);
 
     const err = await createTaskUseCase(dummyCreateTaskRequest).catch((e) => e);
     expect(err).toBeInstanceOf(AppError);
     expect(err.code).toBe(ErrorCode.TASK_NOT_FOUND);
 
-    expect(createTaskItem).toHaveBeenCalledTimes(1);
-    expect(createTaskItem).toHaveBeenCalledWith(dummyCreateTaskRequest);
+    expect(taskRepository.create).toHaveBeenCalledTimes(1);
+    expect(taskRepository.create).toHaveBeenCalledWith(dummyCreateTaskRequest);
 
-    expect(getTaskItemById).toHaveBeenCalledTimes(1);
-    expect(getTaskItemById).toHaveBeenCalledWith(invalidTaskId);
+    expect(taskRepository.getById).toHaveBeenCalledTimes(1);
+    expect(taskRepository.getById).toHaveBeenCalledWith(invalidTaskId);
   });
 });
