f09cba3773942b3870ba497c7d28646f6bf0ee36
ky0yk <40429527+ky0yk@users.noreply.github.com>
2023-09-19 07:02:50 +0900

    インターフェースの命名を見直し

--- functions/src/usecases/contracts/task-repository-contract.ts
+++ functions/src/usecases/contracts/task-repository-contract.ts
@@ -1,20 +1,22 @@
 import { Task } from '../../domain/task';
 
-export type RepoCommand<T, P extends unknown[]> = (...args: P) => Promise<T>;
+export type RepositoryAction<T, P extends unknown[]> = (
+  ...args: P
+) => Promise<T>;
 
-export type CreateTaskCommand = RepoCommand<string, [CreateTaskPayload]>;
-export type FindTaskByIdCommand = RepoCommand<Task | null, [string]>;
-export type UpdateTaskCommand = RepoCommand<
+export type CreateTaskAction = RepositoryAction<string, [CreateTaskPayload]>;
+export type FindTaskByIdAction = RepositoryAction<Task | null, [string]>;
+export type UpdateTaskAction = RepositoryAction<
   Task,
   [string, UpdateTaskAtLeastOne]
 >;
-export type DeleteTaskCommand = RepoCommand<void, [string]>;
+export type DeleteTaskAction = RepositoryAction<void, [string]>;
 
 export type TaskRepository = {
-  create: CreateTaskCommand;
-  findById: FindTaskByIdCommand;
-  update: UpdateTaskCommand;
-  delete: DeleteTaskCommand;
+  create: CreateTaskAction;
+  findById: FindTaskByIdAction;
+  update: UpdateTaskAction;
+  delete: DeleteTaskAction;
 };
 
 export type CreateTaskPayload = {
