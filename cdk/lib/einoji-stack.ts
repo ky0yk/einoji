373f6d9794b4e6395bea603cb7934ac32ae2b5d3
ky0yk <40429527+ky0yk@users.noreply.github.com>
2023-09-21 06:56:05 +0900

    API群を追加

--- cdk/lib/einoji-stack.ts
+++ cdk/lib/einoji-stack.ts
@@ -7,29 +7,85 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 
+const SYSTEM_NAME = 'einoji';
+const ENV_NAME = 'dev';
+
 export class EinojiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const taskTable = new ddb.Table(this, 'TaskTable', {
-      tableName: 'tasks-table',
+    const tasksTable = new ddb.Table(this, 'TasksTable', {
+      tableName: `${SYSTEM_NAME}-${ENV_NAME}-tasks-table`,
       partitionKey: { name: 'userId', type: ddb.AttributeType.STRING },
       sortKey: { name: 'taskId', type: ddb.AttributeType.STRING },
     });
 
-    const getTask = new NodejsFunction(this, 'GetTaskFn', {
-      functionName: 'get-task-fn',
+    const createTaskFn = new NodejsFunction(this, 'CreateTaskFn', {
+      functionName: `${SYSTEM_NAME}-${ENV_NAME}-create-task-fn`,
       runtime: lambda.Runtime.NODEJS_18_X,
       entry: '../functions/src/handlers/create-task-handler.ts',
       handler: 'handler',
+      environment: {
+        TASKS_TABLE_NAME: tasksTable.tableName,
+      },
     });
-    taskTable.grantReadData(getTask);
+    tasksTable.grantReadWriteData(createTaskFn);
 
-    const api = new apigw.HttpApi(this, 'TaskApiGw');
+    const getTaskFn = new NodejsFunction(this, 'GetTaskFn', {
+      functionName: `${SYSTEM_NAME}-${ENV_NAME}-get-task-fn`,
+      runtime: lambda.Runtime.NODEJS_18_X,
+      entry: '../functions/src/handlers/get-task-handler.ts',
+      handler: 'handler',
+      environment: {
+        TASKS_TABLE_NAME: tasksTable.tableName,
+      },
+    });
+    tasksTable.grantReadData(getTaskFn);
+
+    const updateTaskFn = new NodejsFunction(this, 'UpdateTaskFn', {
+      functionName: `${SYSTEM_NAME}-${ENV_NAME}-update-task-fn`,
+      runtime: lambda.Runtime.NODEJS_18_X,
+      entry: '../functions/src/handlers/update-task-handler.ts',
+      handler: 'handler',
+      environment: {
+        TASKS_TABLE_NAME: tasksTable.tableName,
+      },
+    });
+    tasksTable.grantReadWriteData(updateTaskFn);
+
+    const deleteTaskFn = new NodejsFunction(this, 'DeleteTaskFn', {
+      functionName: `${SYSTEM_NAME}-${ENV_NAME}-delete-task-fn`,
+      runtime: lambda.Runtime.NODEJS_18_X,
+      entry: '../functions/src/handlers/delete-task-handler.ts',
+      handler: 'handler',
+      environment: {
+        TASKS_TABLE_NAME: tasksTable.tableName,
+      },
+    });
+    tasksTable.grantReadWriteData(deleteTaskFn);
+
+    const api = new apigw.HttpApi(this, 'TaskApiGw', {
+      apiName: `${SYSTEM_NAME}-${ENV_NAME}-task-api`,
+    });
     api.addRoutes({
-      path: '/tasks/{taskId}',
+      path: '/tasks',
+      methods: [apigw.HttpMethod.POST],
+      integration: new HttpLambdaIntegration('CreateTaskItg', createTaskFn),
+    });
+    api.addRoutes({
+      path: '/tasks/{id}',
       methods: [apigw.HttpMethod.GET],
-      integration: new HttpLambdaIntegration('GetTaskItg', getTask),
+      integration: new HttpLambdaIntegration('GetTaskItg', getTaskFn),
+    });
+    api.addRoutes({
+      path: '/tasks/{id}',
+      methods: [apigw.HttpMethod.PUT],
+      integration: new HttpLambdaIntegration('UpdateTaskItg', updateTaskFn),
+    });
+    api.addRoutes({
+      path: '/tasks/{id}',
+      methods: [apigw.HttpMethod.DELETE],
+      integration: new HttpLambdaIntegration('DeleteTaskItg', deleteTaskFn),
     });
   }
 }
