d6389b3733fda31e490b4fdba05f0f41c02866fc
ky0yk <40429527+ky0yk@users.noreply.github.com>
2023-10-01 22:58:22 +0900

    ドメインルールのコメントを追加

--- functions/src/domain/user/password.ts
+++ functions/src/domain/user/password.ts
@@ -3,11 +3,20 @@ import { UserInvalidPasswordError } from './errors/user-errors';
 
 export const MIN_LENGTH = 8;
 export const MAX_LENGTH = 64;
+
 export const UPPERCASE_PATTERN = /[A-Z]/;
 export const LOWERCASE_PATTERN = /[a-z]/;
 export const NUMBER_PATTERN = /[0-9]/;
 export const SPECIAL_CHAR_PATTERN = /[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]+/;
 
+/*
+ * パスワード要件:
+ * 8文字以上64文字以下
+ * 少なくとも1つの大文字を含む
+ * 少なくとも1つの小文字を含む
+ * 少なくとも1つの数字を含む
+ * 少なくとも1つの特殊文字（記号）を含む
+ */
 const passwordSchema = z
   .string()
   .min(MIN_LENGTH, `Password must be at least ${MIN_LENGTH} characters long.`)
