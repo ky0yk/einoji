7d8ea1a84c627597d45ef56d1dfaba6adf52a56d
ky0yk <40429527+ky0yk@users.noreply.github.com>
2023-10-01 23:15:16 +0900

    ユースケースを実装

--- functions/src/usecases/users/contracts/user-repository-contract.ts
+++ functions/src/usecases/users/contracts/user-repository-contract.ts
@@ -3,12 +3,19 @@ import { Password } from '../../../domain/user/password';
 import { RepositoryAction } from '../../base/contract/base-contracts';
 
 export type CreateUserAction = RepositoryAction<string, [CreateUserPayload]>;
+export type AuthUserAction = RepositoryAction<string, [AuthUserPayload]>;
 
 export type UserRepository = {
   create: CreateUserAction;
+  auth: AuthUserAction;
 };
 
 export type CreateUserPayload = {
+  email: Email;
+  password: Password;
+};
+
+export type AuthUserPayload = {
   email: Email;
   password: Password;
 };
