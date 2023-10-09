a54b6d92babafceb59b1c9083553ccc58d0111da
ky0yk <40429527+ky0yk@users.noreply.github.com>
2023-10-09 13:24:39 +0900

    ファイル名のtypoを修正

--- functions/src/handlers/users/auth-user-handler.ts
+++ functions/src/handlers/users/auth-user-handler.ts
@@ -6,7 +6,7 @@ import {
 import { validateBody } from '../base/http/validators';
 import { httpResponse } from '../base/http/http-response';
 import { HttpStatus } from '../base/http/http-status';
-import { authUserUsecase } from '../../usecases/users/auth-user-usecaset';
+import { authUserUsecase } from '../../usecases/users/auth-user-usecase';
 import { AuthUserRequestSchema } from './schemas/user-requests';
 
 const authUserHandler: RequestHandlerWithoutContext = async (
